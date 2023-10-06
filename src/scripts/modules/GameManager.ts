fdc18548d79456752e7b14a6e59d37302a97d93c
dCben335 <benoit.cabocel.335@gmail.com>
2023-10-06 09:20:00 +0200

    Create who plays  methods

--- src/scripts/modules/GameManager.ts
+++ src/scripts/modules/GameManager.ts
@@ -15,8 +15,8 @@ export default class GameManager {
   gameContainer: HTMLDivElement;
 
   round: number;
-  activePlayers: Player[] | undefined;
-  whoIsPlaying: number | undefined;
+  activePlayers: number[] | undefined;
+  whoIsPlaying: number;
   pattern: string[];
 
   colorPossibilities: { [key: string]: string } = {
@@ -33,7 +33,7 @@ export default class GameManager {
 
     this.activePlayers;
     this.round = 1;
-    this.whoIsPlaying;
+    this.whoIsPlaying = -1;
     this.pattern = [];
     this.players = this.setPlayers(playersName);
 
@@ -43,21 +43,142 @@ export default class GameManager {
   startGame() {
     this.setActivePlayers(this.players);
     this.generateHTML();
-    this.createPattern(5);
+    this.countdown();
+  }
+
+  turnTransition() {
+    this.disableButtons();
+    this.countdown();
   }
   
+  disableButtons() {
 
+    document.querySelectorAll('.game').forEach((container) => {
+      if (container.classList.contains('playing')) {
+        container.classList.remove('playing');
+      } 
 
-  creationTransition() {
-    this.countdown()
+      container.querySelectorAll('.game-buttons button').forEach((button) =>
+        button.removeEventListener('click', () => console.log("dqdqz")) 
+      )
+    })
   }
 
-  setWhoIsPlayings(): number {
-    if (!this.activePlayers) return this.whoIsPlaying = 0
+  enableButtons(playerNumber: number) {
+    const currentPlayerPlaying = document.querySelector(`.game.player-${playerClasses[playerNumber + 1]}`) as HTMLDivElement;
+    currentPlayerPlaying?.classList.add('playing');
 
-    const index = Math.floor(Math.random() * this.activePlayers.length);
+    currentPlayerPlaying?.querySelectorAll(`.game-buttons button`).forEach((button) => {
+      button.addEventListener('click', () => console.log((button as HTMLButtonElement)?.dataset.color));
+    })
+  }
 
-    return (this.whoIsPlaying = index);
+  setWhoIsPlayings(): void {
+    if (!this.activePlayers) return
+
+    if (this.whoIsPlaying === -1) {
+
+      this.whoIsPlaying = Math.floor(Math.random() * this.activePlayers.length);
+
+    } else {
+      this.whoIsPlaying = this.whoIsPlaying + 1 > this.activePlayers.length -1 ? this.activePlayers[0] : this.activePlayers[this.whoIsPlaying + 1]; 
+    }
+    
+    this.showWhoIsPlaying()
+  }
+
+  showWhoIsPlaying() {
+    const currentPlayer = document.querySelector('.game-indications > h2') as HTMLHeadingElement
+
+    currentPlayer.textContent = this.players.length === 1 
+      ? `à toi de jouer ${this.players[this.whoIsPlaying].name}`
+      : `à ${this.players[this.whoIsPlaying].name} de jouer`;
+
+    this.enableButtons(this.whoIsPlaying);
+  }
+
+
+
+  countdown() {
+    let counter: number = 3;
+    const indications = this.gameContainer.querySelector('.game-indications h2') as HTMLHeadingElement
+
+    const countdownInterval = setInterval(() => {
+      indications.textContent = counter > 0 ? `${counter}` : "C'est parti !"
+        
+      if ( counter === -2 ) {
+        indications.textContent = "";
+        this.clearCountdown(countdownInterval);
+        this.createPattern(5);
+      }
+
+      counter--;
+    }, 1000);
+  }
+
+
+  createPattern(nbColorCreated: number): void {
+    const colors: string[] = Object.keys(this.colorPossibilities);
+
+    const patternTab: Array<string> = Array(nbColorCreated)
+      .fill("")
+      .map(() => colors[Math.floor(Math.random() * colors.length)]);
+
+    this.pattern = [...this.pattern, ...patternTab];
+
+    this.playPattern();
+  }
+
+  playPattern() {
+    const synth = new Tone.Synth().toDestination()
+    let index: number = 0;
+
+    const playingNotes = setInterval(() => {
+      const buttonColor = document.querySelectorAll(`[data-color="${this.pattern[index]}"]`);
+
+      buttonColor.forEach((button) => button?.classList.add("activeColor"));
+
+      const note: string = this.colorPossibilities[this.pattern[index]];
+      synth.triggerAttackRelease(note, "4n");
+
+      setTimeout(() => {
+        buttonColor.forEach((button) => button?.classList.remove("activeColor"))
+      },  this.gameSpeed / 1.75);
+
+      index++;
+      
+      if (index >= this.pattern.length + 1) {
+        clearInterval(playingNotes);
+        this.setWhoIsPlayings();
+      }
+
+    }, this.gameSpeed);
+  }
+
+  setPlayers(playersName: string[]): Player[] {
+    return playersName.map((playerName) => {
+      return {
+        name: playerName,
+        score: 0,
+        round: this.round || 1,
+      };
+    });
+  }
+
+  clearCountdown( interval: ReturnType<typeof setInterval>): ReturnType<typeof clearInterval> {
+    return clearInterval(interval);
+  }
+
+  setGameSpeed(speed: number): number {
+    return (this.gameSpeed = speed);
+  }
+
+  setActivePlayers(players: Player[]): number[] {
+    return (this.activePlayers = players.map((el: Player, index: number) => index));
+  }
+
+  setRound(round: number): number {
+    return (this.round = round);
   }
 
   generateHTML(): void {
@@ -79,120 +200,29 @@ export default class GameManager {
         </div>
     `;
 
+    const gamesIndications = `
+      <div class="game-indications">
+        <h2></h2>
+      </div>
+    `
+
     this.players.forEach((element: Player, index: number) => {
       this.gameContainer.innerHTML += `
           <div class="game player-${playerClasses[index + 1]}">
-            ${this.players.length === 1 ? playerInfos : ""}
+            ${this.players.length === 1 ? playerInfos : gamesIndications}
             <section class="game-board">
                 <div class="game-circle">
-                  ${
-                    this.players.length === 1
-                      ? `
-                      <div class="game-indications">
-                        <h2></h2>
-                      </div>
-                      `
-                      : playerInfos
-                  }
+                  ${ this.players.length === 1 ? gamesIndications: playerInfos }
                 </div>
                 <div class="game-buttons">
                     ${Object.keys(this.colorPossibilities)
-                      .map(
-                        (color) =>
+                      .map( (color) =>
                           `<button data-color="${color}" style="--_button-color: var(--${color})"></button>`
-                      )
-                      .join("")}
+                      ).join("")}
                 </div>
             </section>       
           </div> 
       `;
     });
-  }
-
-  countdown() {
-    let counter: number = 3;
-    const indications = this.gameContainer.querySelector('.game-indications h2') as HTMLHeadingElement
-
-    const countdownInterval = setInterval(() => {
-      indications.textContent = counter > 0 ? `${counter}` : "C'est parti !"
-        
-      if ( counter === -2 ) {
-        indications.textContent = "";
-        this.clearCountdown(countdownInterval);
-        this.
-      }
-
-      counter--;
-    }, 1000);
-  }
-
-
-  createPattern(nbColorCreated: number): void {
-    const colors: string[] = Object.keys(this.colorPossibilities);
-
-    const patternTab: Array<string> = Array(nbColorCreated)
-      .fill("")
-      .map(() => colors[Math.floor(Math.random() * colors.length)]);
-
-    this.pattern = [...this.pattern, ...patternTab];
-
-    this.playPattern();
-  }
-
-  playPattern() {
-    const synth = new Tone.Synth().toDestination();
-    const now = Tone.now();
-
-    let index: number = 0;
-
-    const playingNotes = setInterval(() => {
-
-      const buttonColor = document.querySelectorAll(
-        `[data-color="${this.pattern[index]}"]`
-      );
-
-      buttonColor.forEach((button) => button?.classList.add("activeColor"));
-
-      const note: string = this.colorPossibilities[this.pattern[index]];
-      synth.triggerAttackRelease(note, "4n");
-
-      setTimeout(() => {
-        buttonColor.forEach((button) => button?.classList.remove("activeColor"))
-      },  this.gameSpeed / 1.75);
-
-      index++;
-      
-      if (index >= this.pattern.length + 1) {
-        clearInterval(playingNotes);
-        this.setWhoIsPlayings();
-      }
-
-    }, this.gameSpeed);
-  }
-
-  clearCountdown( interval: ReturnType<typeof setInterval>): ReturnType<typeof clearInterval> {
-    return clearInterval(interval);
-  }
-
-  setPlayers(playersName: string[]): Player[] {
-    return playersName.map((playerName) => {
-      return {
-        name: playerName,
-        score: 0,
-        round: this.round || 1,
-      };
-    });
-  }
-
-  setGameSpeed(speed: number): number {
-    return (this.gameSpeed = speed);
-  }
-
-  setActivePlayers(players: Player[]): Player[] {
-    return (this.activePlayers = players);
-  }
-
-  setRound(round: number): number {
-    return (this.round = round);
   }
 }
