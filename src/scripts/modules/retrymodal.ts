6fda78abadc8c68c73633b69c762630ae6101750
dCben335 <benoit.cabocel.335@gmail.com>
2023-10-23 16:29:19 +0200

    Merge endGame

--- src/scripts/modules/retrymodal.ts
+++ src/scripts/modules/retrymodal.ts
@@ -2,37 +2,31 @@ const retry = document.querySelector(".retry") as HTMLDivElement;
 
 function showPartyRecap(players: any) {
   const templateModal = `
-    <div class="animated-rgb modal">
-    <div>
-    <div class="modal-content" >
-
-    <h3>Fin de partie</h3>
-    <div>
-    ${
-      Array.isArray(players)
-        ? players
-            .map(
-              ({ gamertag, score, round }: any, index) => `
+    <aside class="animated-rgb modal">
+      <div>
+        <div class="modal-content" >
+          <h3>Fin de partie</h3>
+          <div>
+          ${Array.isArray(players) ? players.map(({ gamertag, score, round }: any, index: number) => `
               <div class="scoreboardRow" >
-              <span class="rank">#${index + 1}</span>
-              <span class="gamertag">${gamertag}</span>
-              <span class="score">${score}</span>
+                <span class="rank">#${index + 1}</span>
+                <span class="gamertag">${gamertag}</span>
+                <span class="score">${score}</span>
+              </div>
+            `).join("") : "<p>Aucun joueur</p>"
+          }
           </div>
-    `
-            )
-            .join("")
-        : "<p>Aucun joueur</p>"
-    }
-    </div>
-    <div>
-    <button>Menu</button>
-    <button>Réessayer</button>
-    </div>
-    </div>
-    </div>
-    </div>
+          <div>
+            <button>Menu</button>
+            <button>Réessayer</button>
+          </div>
+        </div>
+      </div>
+    </aside>
     `;
   retry.innerHTML = templateModal;
 }
 
-export { showPartyRecap };
+export { 
+  showPartyRecap 
+};
